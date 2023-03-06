f83ea1c405a3eb928566bc70ed0e6dccf4a3733d
qShadxw <tyl@null.net>
2023-03-06 11:23:54 +0000

    Edit Readme

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-  phpinfo();
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>My Page</title>
+  </head>
+  <body>
+      <h1>My Page</h1>
+      <p>My Page is the best page.</p>
+  </body>
+</html>
