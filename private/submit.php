206b71bb235cae3ce192ee1f6ed553ce9f4a7161
Tyler Davies <45601586+qShadxw@users.noreply.github.com>
2023-03-06 14:58:07 +0000

    Update submit.php

--- private/submit.php
+++ private/submit.php
@@ -1,5 +1,9 @@
 <?php
 
+$apiKey = "48697636dc984d3a7e4b10eb28a1415a";
+$cityId = "";
+$googleApiUrl = "https://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+
 echo '
 <span class="report">
     <span>City: Town</span>
